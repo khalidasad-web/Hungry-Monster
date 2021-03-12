8766d4f1ad2335a9012c13b700c4a5f5138a7903
khalidasad-web <khalid.ibnal.asad@gmail.com>
2021-03-12 11:23:46 +0000

    3rd-Commit-12March2021

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-function fetchMealInfo(){
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+const fetchMealInfo = () => {
+    const searchInput = document.getElementById('search-meal').value.toLowerCase();
+    const searchLtr = searchInput.charAt(0);
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchLtr}`)
         .then(response => response.json())
-        .then(data => console.log(data));
-
+        .then(data => displayMealInfo(data))
+        .catch(error => {
+            const errorMsg = `
+            <h4>Name: No meal is found in the database....</h4>
+            `;
+            document.getElementById('display-meal-details').innerHTML = errorMsg;
+        });
+}
+const displayMealInfo = meal => {
+    const mealsContainer = document.getElementById('meals-container');
+    const mealArray = meal.meals;
+    mealArray.forEach(element => {
+        const mealContainer = document.createElement('div');
+        mealContainer.className = 'meal-container';
+        const mealInfo = `
+            <img onclick="displayMealDetails(${element.idMeal})" src=${element.strMealThumb} alt="">
+            <p>${element.strMeal}</p>`;
+        mealContainer.innerHTML = mealInfo;
+        mealsContainer.appendChild(mealContainer);
+    });
+}
+const displayMealDetails = id => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const mealDetail = document.getElementById('display-meal-details');
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const meal = `
+                    <h4>Name: ${data.meals[0].strMeal}</h4>
+                    <p>Category: ${data.meals[0].strCategory}</p>
+                    <p>Area: ${data.meals[0].strArea}</p>
+                    <p>Instruction: ${data.meals[0].strInstructions}</p>
+                    `;
+            mealDetail.innerHTML = meal;
+        });
 }
